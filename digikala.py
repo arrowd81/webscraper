5e9665a2e33ef7bd9043e354177bd64c99e4e472
arrowd81 <arashd1381@gmail.com>
2023-06-06 18:59:57 +0330

    works this time :)

--- digikala.py
+++ digikala.py
@@ -5,39 +5,51 @@ import time
 
 webdriver_path = "E:/Projects/webscraping/chromedriver.exe"
 out=[]
-gonext = True
+nextpage = True
+nextprice = True
 minPrice = 1
-maxPrice = 5000000
+maxPrice = 500000
+while nextprice:
+    pagenum = 1
+    while nextpage:
+        url = f"https://www.digikala.com/search/category-mobile-phone/product-list/?page={pagenum}&price%5Bmax%5D={maxPrice}0&price%5Bmin%5D={minPrice}0"
 
-# while gonext:
+        # Configure the web driver
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(executable_path=webdriver_path, options=options)
+        # Load the webpage
+        driver.get(url)
 
-url = "https://www.digikala.com/search/mobile-phone/?page=1"
+        time.sleep(5)
 
-# Configure the web driver
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(executable_path=webdriver_path, options=options)
-# Load the webpage
-driver.get(url)
+        # Get the page source after JavaScript rendering
+        page_source = driver.page_source
 
-time.sleep(15)
-# Get the page source after JavaScript rendering
-page_source = driver.page_source
+        # Create a BeautifulSoup object to parse the HTML content
+        soup = BeautifulSoup(page_source, "html.parser")
+        # Find all the anchor tags (<a>) in the parsed HTML
 
-# Create a BeautifulSoup object to parse the HTML content
-soup = BeautifulSoup(page_source, "html.parser")
-# Find all the anchor tags (<a>) in the parsed HTML
+        anchor_tags = soup.find_all("a" , class_ = "d-block pointer pos-relative bg-000 overflow-hidden grow-1 py-3 px-4 px-2-lg h-full-md styles_VerticalProductCard--hover__ud7aD")
 
-anchor_tags = soup.find_all("a" , class_ = "d-block pointer pos-relative bg-000 overflow-hidden grow-1 py-3 px-4 px-2-lg h-full-md styles_VerticalProductCard--hover__ud7aD")
+        # Extract the href attribute from each anchor tag
+        links = [a.get("href") for a in anchor_tags if a.get("href")]
 
-# Extract the href attribute from each anchor tag
-links = [a.get("href") for a in anchor_tags if a.get("href")]
+        if len(links) < 20:
+            nextpage = False
+        else:
+            pagenum += 1
+        
+        # Print all the links
+        for link in links:
+            out.append(link)
+        # Close the web driver
+        driver.quit()
 
-if len(links) < 20:
-    gonext = False
-# Print all the links
-for link in links:
-    out.append(link)
-# Close the web driver
-driver.quit()
+    if maxPrice < 10000000 :
+        minPrice += 500000
+        maxPrice += 500000
+        nextpage = True
+    else:
+        nextprice = False
 
 print(len(out))
