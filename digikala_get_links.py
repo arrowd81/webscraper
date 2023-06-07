10a541391d4247917e32c0f0b385c83565aba684
arrowd81 <arashd1381@gmail.com>
2023-06-07 19:43:32 +0330

    tamiz shode

--- digikala_get_links.py
+++ digikala_get_links.py
@@ -14,29 +14,22 @@ writer = csv.writer(linkfile , lineterminator='\n' ,)
 while nextprice:
     pagenum = 1
     while nextpage:
-        # url = f"https://www.digikala.com/search/category-mobile-phone/product-list/?page={pagenum}&price%5Bmax%5D={maxPrice}0&price%5Bmin%5D={minPrice}0"
         url = f"https://api.digikala.com/v1/categories/mobile-phone/search/?price%5Bmax%5D={maxPrice}0&price%5Bmin%5D={minPrice}0&seo_url=%2Fcategory-mobile-phone%2Fproduct-list%2F%3Fprice%255bmax%255d%3D20000000%26price%255bmin%255d%3D0&page={pagenum}"
 
-
-        # Load the webpage
         response = requests.get(url)
 
-        # Get the page source after JavaScript rendering
         data = response.json()
         
         products = data['data']['products']
-
 
         if len(products) < 20:
             nextpage = False
         else:
             pagenum += 1
         
-        # Print all the links
         for product in products:
             link = product["url"]["uri"]
             writer.writerow([link])
-
 
     if maxPrice <= 100000000 :
         minPrice += 500000
