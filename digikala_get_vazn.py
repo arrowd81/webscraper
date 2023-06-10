4588f6cba529f5b98b39d388891ef51e79286190
arrowd81 <arashd1381@gmail.com>
2023-06-10 16:08:30 +0330

    finished i think :)

--- digikala_get_vazn.py
+++ digikala_get_vazn.py
@@ -1,56 +1,41 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import time
+import requests
+import json
 import csv
 
 def getVazn(url):
-  arrayout = ["",""]
+  arrayout = ["","<==name vanz==>",""]
 
-  webdriver_path = "E:/Projects/webscraping/chromedriver.exe"
+  response = requests.get(url)
 
-  options = webdriver.ChromeOptions()
-
-  driver = webdriver.Chrome(executable_path=webdriver_path, options=options)
-
-  driver.get(url)
-
-  time.sleep(10)
-
-  page_source = driver.page_source
-
-  soup = BeautifulSoup(page_source, "html.parser")
+  data = response.json()
 
   #get name
-  itemname = soup.find("h1" , class_ = "text-h4 color-900 mb-2 disable-events").text
+  itemname = data['data']['product']['title_fa']
   arrayout[0] = itemname
 
-
   #getvazn
-  infodiv = soup.find("div" , class_ = "w-full w-auto-lg grow-1")
-  infoitem = infodiv.find_all("div" , class_ = "w-full d-flex last styles_SpecificationAttribute__valuesBox__gvZeQ")
+  itemvazn = data['data']['product']['specifications'][0]['attributes']
+  arrayout[2] = itemvazn[1]['values'][0]
 
-  title = infodiv.find_all("p" , class_ = "ml-4 text-body-1 color-500 py-2 py-3-lg p-2-lg shrink-0 styles_SpecificationAttribute__value__CQ4Rz")
-
-  for i , item in enumerate(title):
-    if item.text == "وزن":
-      arrayout[1] = (infoitem[i].find("p" , class_ = "d-flex ai-center w-full text-body-1 color-900 break-words").text)
-
-  driver.quit()
   return arrayout
 
-# print(getVazn("https://www.digikala.com/product/dkp-11410115/گوشی-موبایل-جی-ال-ایکس-مدل-2690-goldmini-plus-دو-سیم-کارت/"))
-
+# print(getVazn("https://api.digikala.com/v1/product/352451/"))
 # Specify the file path
 file_path = "links.csv"
 pruduct_path = "result.csv"
-headurl = "https://www.digikala.com"
-with open(pruduct_path, 'w', newline='' , encoding='utf-8') as csvfile:
-  writer = csv.writer(csvfile)
+headurl = "https://api.digikala.com/v1/product/"
+with open(pruduct_path, 'w', newline='' , encoding='utf-8') as csvfilew:
+  writer = csv.writer(csvfilew)
   # Open the file in read mode
-  with open(file_path, 'r') as csvfile:
-    reader = csv.reader(csvfile)
+  with open(file_path, 'r' , encoding='utf-8') as csvfiler:
+    reader = csv.reader(csvfiler)
     # Read each row of the CSV file
+    # rownumber = len(list(reader))
     for row in reader:
       if row[0].startswith("/"):
-        url = (headurl + row[0])
+        product_id_end = row[0].find("/", 13) + 1
+        url = (headurl + row[0][13:product_id_end])
         writer.writerow(getVazn(url))
+      print(f"getting item number {reader.line_num}")
+
+
