18464aea27b8818df853689f73034b06152e8b5d
arrowd81 <arashd1381@gmail.com>
2023-06-24 17:59:22 +0330

    :((

---
+++ config.py
@@ -0,0 +1,5 @@
+links_path = 'links.csv'
+result_path = 'result.csv'
+min_price = 0
+max_price = 100000000
+price_interval = 500000
--- digikala_get_vazn.py
+++ digikala_get_vazn.py
@@ -1,8 +1,10 @@
 import requests
 import json
 import csv
+import config
+import constant
 
-def getVazn(url):
+def get_weight(url):
   arrayout = ["",""]
 
   response = requests.get(url)
@@ -13,7 +15,7 @@ def getVazn(url):
   itemname = data['data']['product']['title_fa']
   arrayout[0] = itemname
 
-  #getvazn
+  #get weight
   item_attributes = data['data']['product']['specifications'][0]['attributes']
   itemvazn = ""
   found = False
@@ -29,21 +31,27 @@ def getVazn(url):
 
   return arrayout
 
-# print(getVazn("https://api.digikala.com/v1/product/352451/"))
-# Specify the file path
-file_path = "links.csv"
-pruduct_path = "result.csv"
-headurl = "https://api.digikala.com/v1/product/"
-with open(pruduct_path, 'w', newline='' , encoding='utf-8') as csvfilew:
-  writer = csv.writer(csvfilew)
-  # Open the file in read mode
-  with open(file_path, 'r' , encoding='utf-8') as csvfiler:
+def read_links():
+  links = []
+  with open(config.links_path, 'r' , encoding='utf-8') as csvfiler:
     reader = csv.reader(csvfiler)
-    # Read each row of the CSV file
-    # rownumber = len(list(reader))
     for row in reader:
       if row[0].startswith("/"):
-        product_id_end = row[0].find("/", 13) + 1
-        url = (headurl + row[0][13:product_id_end])
-        writer.writerow(getVazn(url))
-      print(f"getting item number {reader.line_num}")
+        links.append(row)
+  return links
+
+def write_data(links):
+  all_data = []
+  for i, link in enumerate(links):
+    product_id_end = link[0].find("/", 13) + 1
+    url = (constant.head_url + link[0][13:product_id_end])
+    all_data.append(get_weight(url))
+    print(f"getting item number {i}")
+
+  with open(config.result_path, 'w', newline='' , encoding='utf-8') as csvfilew:
+    writer = csv.writer(csvfilew)
+    for data in all_data:
+      writer.writerow(data)
+
+if __name__ == "__main__":
+  write_data(read_links())
